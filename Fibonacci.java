a10f3039e525641ac90521e8aee09e1fe447dae1
Sandy Le <lvl25sandy@gmail.com>
2019-02-04 16:27:35 -0500

    updated file w/ guava lib

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,58 +1,75 @@
 import java.util.Scanner;
+import com.google.common.base.Stopwatch;
 
-
+// Citation: Tutorial and usage of timing library "Guava - Stopwatch" https://google.github.io/guava/releases/18.0/api/docs/com/google/common/base/Stopwatch.html
 
 public class Fibonacci {
 
+    static long rec = 0;
 
     public static void main (String[] args) {
         // creation of a scanner that takes system input, in this case, the nth fibonacci times.
         Scanner getInput = new Scanner(System.in);
+
         // Prints out to have user input
         System.out.println("Enter a number for the fibonacci sequence: ");
+
         // creation of int maxNum, that is the max times the fibonacci sequence executes for, inserted by the user.
         int maxNum = getInput.nextInt();
+
+        Stopwatch stopwatchIte = Stopwatch.createStarted();
         // Inserts the user input into the method that uses a for loop to calculate the fibonacci sequence.
         System.out.println(fiboSeriesIte(maxNum));
-        System.out.println("-----------------------------");
-        System.out.println(fiboSeriesRec(maxNum));
+
+        // Stops timer for Iteration method
+        stopwatchIte.stop();
+
+        // Declaration and creation of stopwatch timer starting.
+        Stopwatch stopwatchRec = Stopwatch.createStarted();
+        // Just a title for the recursive method output.
+        System.out.println("\n[(Recursive) Fibonacci sequence for n = "+ maxNum +"]");
+
+        // for loop that prints each number in the fibonacci series in the rec method up to user input 'maxNum'
+        for (long i=0; i<maxNum; i++){
+            System.out.println(fiboSeriesRec(i));
+        }
+        // Stops timer for recursion method
+        stopwatchRec.stop();
+
+        // Prints out the outcome of both method's time.
+        System.out.println("\nIteration method toke: "+stopwatchIte);
+        System.out.println("Recursion method toke: "+stopwatchRec);
     }
 
-    public static int fiboSeriesRec(int num) {
-        int prevNum = 0;
-        int nextNum = 1;
-        int sum;
-        if (num == 0) {
-            // Spits back an error for zero input
-            return 0;
-        } else if (num == 1) {
-            return 1;
-        } else {
-            System.out.println(prevNum);
-            sum = prevNum + nextNum;
-            prevNum = nextNum;
-            prevNum = sum;
-            return fiboSeriesIte(num);
-        }
-}
+    public static long fiboSeriesRec(long n){
 
-    // The Interation method that uses a for loop to calculate the fibonacci numbers up to a user input nth times.
-    static int fiboSeriesIte(int n){
+        if(n ==0){
+            return 0;
+        } else if (n == 1){
+            return 1;
+        }
+        else{
+            return fiboSeriesRec(n-1)+fiboSeriesRec(n-2);
+        }
+    }
+
+    // The Iteration method that uses a for loop to calculate the fibonacci numbers up to a user input nth times.
+    public static long fiboSeriesIte(long n){
         // Declaration of integers for the different ints used to calculate the fibonacci series, oldNum is the first number in this case 0, and 1 is the second.
-        int oldNum = 0;
-        int newNum = 1;
-        int result;
+        long oldNum = 0;
+        long newNum = 1;
+        long result;
         // if statement, depending on user input to calculate fibonacci
         if (n==0){
             // Spits back an error for zero input
-            System.out.println("***ERROR***: Zero input gives you nothing!");
+            return 0;
         } else if (n==1){
             // Prints the fibonacci sequence for the input 1.
-            System.out.println("\n[Fibonacci sequence for n = "+ n +"]");
+            System.out.println("\n[(Iterative) Fibonacci sequence for n = "+ n +"]");
             return 0;
         } else{
             // Prints a header for the fibonacci sequence for what the user inputs.
-            System.out.println("\n[Fibonacci sequence for n = "+ n +"]");
+            System.out.println("\n[(Iterative) Fibonacci sequence for n = "+ n +"]");
 
             // For loop that calculates the next sum of the previous two sums, also known as the fibonacci series up to a user input 'n' times.
             for (int i=1; i< n; i++){
